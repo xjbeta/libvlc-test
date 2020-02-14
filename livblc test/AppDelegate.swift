37117912e27514e2fc4b78c4c7b25b8da8c77b23
xjbeta <xjbeta.tiny@gmail.com>
2020-02-14 22:47:30 +0800

    Initial Commit

---
+++ livblc test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  livblc test
+//
+//  Created by xjbeta on 2020/2/14.
+//  Copyright © 2020 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ livblc test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  livblc test
+//
+//  Created by xjbeta on 2020/2/14.
+//  Copyright © 2020 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
